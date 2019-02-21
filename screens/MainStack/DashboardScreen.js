ca116363da1f5bb530a7c4c6ba66c1abcfb87e93
LimWJie <wjie_lim@outlook.com>
2019-02-21 16:50:44 +0800

    Added header title for Dashboard Screen

--- screens/MainStack/DashboardScreen.js
+++ screens/MainStack/DashboardScreen.js
@@ -1,7 +1,15 @@
 import React, { Component } from "react";
 import { View, Text } from "react-native";
 
+// temp variables for storing city and country desired for weather forecast
+const city = "Singapore";
+const country = "Singapore";
+
 export default class DashboardScreen extends Component {
+  static navigationOptions = {
+    title: `${city}, ${country}`
+  };
+
   render() {
     return (
       <View>
