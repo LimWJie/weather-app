5d2c0f29c4a992508b75052c5a2670123170c5d0
LimWJie <wjie_lim@outlook.com>
2019-02-21 16:45:55 +0800

    Style header by react-navigation's navigation options

--- App.js
+++ App.js
@@ -1,9 +1,14 @@
 import React from "react";
+import { StatusBar } from "react-native";
 import { createAppContainer } from "react-navigation";
 import RootNavigator from "./screens/RootNavigator";
 
 const AppContainer = createAppContainer(RootNavigator);
 export default class App extends React.Component {
+  componentDidMount() {
+    StatusBar.setBarStyle("light-content");
+  }
+
   render() {
     return <AppContainer />;
   }
--- screens/MainStack/index.js
+++ screens/MainStack/index.js
@@ -1,6 +1,16 @@
 import { createStackNavigator } from "react-navigation";
 import DashboardScreen from "./DashboardScreen";
 
-export default createStackNavigator({
-  Dashboard: DashboardScreen
-});
+export default createStackNavigator(
+  {
+    Dashboard: DashboardScreen
+  },
+  {
+    defaultNavigationOptions: {
+      headerStyle: {
+        backgroundColor: "#EE4D3B"
+      },
+      headerTintColor: "#FFFFFF"
+    }
+  }
+);
