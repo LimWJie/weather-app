c306f0f15fb0873e8c7cc7ba58812781e77e33f1
LimWJie <wjie_lim@outlook.com>
2019-02-22 13:48:33 +0800

    Modified formatDate helper func to expect UNIX timestamp in seconds instead of milliseconds

--- utils/helpers/index.js
+++ utils/helpers/index.js
@@ -1,8 +1,8 @@
 import moment from "moment";
 
 /**
- * Convert UNIX timestamp into human readable datetime string
- * @param {String} timestamp UNIX timestamp to be formatted
+ * Convert UNIX timestamp in seconds into human readable datetime string
+ * @param {String} timestamp UNIX timestamp in seconds to be formatted
  */
 export const formatDate = timestamp =>
-  moment(timestamp).format("ddd, D MMM YYYY hh:mm A");
+  moment.unix(timestamp).format("ddd, D MMM YYYY hh:mm A");
